59a44dfc6dfc1943d37874d6887f832730a0da41
DY12345 <s865529248@163.com>
2020-04-21 14:52:09 +0800

    完成header

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,10 @@
   <el-container class="home-container">
     <!-- 头部 -->
     <el-header>
-      Header
+      <div>
+        <img src="../assets/heima.png" alt />
+        <span>电商后台管理系统</span>
+      </div>
       <el-button type="info" @click="logout()">退出</el-button>
     </el-header>
     <!-- 页面主体区域 -->
@@ -30,14 +33,27 @@ export default {
 <style lang="less" scoped>
 .el-header {
   background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
 }
 .el-aside {
   background-color: #333744;
 }
-.el-main{
+.el-main {
   background-color: #eaedf1;
 }
-.home-container{
+.home-container {
   height: 100%;
 }
 </style>
