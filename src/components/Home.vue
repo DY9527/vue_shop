ecf5573eb44199d6195944a74b68b70f4313c6b8
DY12345 <s865529248@163.com>
2020-04-21 14:37:29 +0800

    home页面整体布局

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,18 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout()">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部 -->
+    <el-header>
+      Header
+      <el-button type="info" @click="logout()">退出</el-button>
+    </el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">Aside</el-aside>
+      <!-- 右侧主体 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 <script>
 export default {
@@ -17,4 +28,16 @@ export default {
 }
 </script>
 <style lang="less" scoped>
+.el-header {
+  background-color: #373d41;
+}
+.el-aside {
+  background-color: #333744;
+}
+.el-main{
+  background-color: #eaedf1;
+}
+.home-container{
+  height: 100%;
+}
 </style>
