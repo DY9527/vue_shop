0225e55f2e3008006d267e64cc124f54ad1cae07
DY12345 <s865529248@163.com>
2020-05-08 21:28:32 +0800

    商品分类表格框架搭好了

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -13,6 +13,7 @@
         </el-col>
       </el-row>
       <tree-table
+      class="treeTable"
         :data="catelist"
         :columns="columns"
         :selection-type="false"
@@ -20,8 +21,40 @@
         show-index
         index-text="#"
         border
-        :show-row-hover='false'
-      ></tree-table>
+        :show-row-hover="false"
+      >
+      <!-- 有效  -->
+        <template slot="isok" slot-scope="scope">
+          <i
+            class="el-icon-success"
+            style="color: lightgreen"
+            v-if="scope.row.cat_deleted === false"
+          ></i>
+          <i class="el-icon-error" style="color:red" v-else></i>
+        </template>
+        <!-- 排序 -->
+        <template slot="order" slot-scope="scope">
+          <el-tag size="mini" v-if="scope.row.cat_level===0">一级</el-tag>
+          <el-tag size="mini" v-else-if="scope.row.cat_level===1" type="success">二级</el-tag>
+          <el-tag size="mini" v-else type="warning">三级</el-tag>
+        </template>
+        <!-- 操作 -->
+        <template slot="opt" slot-scope="scope">
+          <el-button type="primary" size="mini" icon="el-icon-edit" :value="scope.row.cat_level">编辑</el-button>
+          <el-button type="danger" size="mini" icon="el-icon-delete">删除</el-button>
+        </template>
+      </tree-table>
+
+      <!-- 分页区域 -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[3, 5, 10, 15]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      ></el-pagination>
     </el-card>
   </div>
 </template>
@@ -42,6 +75,27 @@ export default {
         {
           label: '分类名称',
           prop: 'cat_name'
+        },
+        {
+          label: '是否有效',
+          // 将当前列定义为模板列
+          type: 'template',
+          // 表示当前着一列使用的模板名称
+          template: 'isok'
+        },
+        {
+          label: '排序',
+          // 将当前列定义为模板列
+          type: 'template',
+          // 表示当前着一列使用的模板名称
+          template: 'order'
+        },
+        {
+          label: '操作',
+          // 将当前列定义为模板列
+          type: 'template',
+          // 表示当前着一列使用的模板名称
+          template: 'opt'
         }
       ]
     }
@@ -60,10 +114,25 @@ export default {
       this.catelist = res.data.result
       this.total = res.data.total
       this.queryInfo.pagenum = 1
+    },
+    // 监听 pagesize 改变的事件
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`)
+      this.queryInfo.pagesize = val
+      this.getCateList()
+    },
+    // 监听 页码值 改变的事件
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`)
+      this.queryInfo.pagenum = val
+      this.getCateList()
     }
   }
 }
 </script>
 
 <style lang=less scoped>
+.treeTable{
+  margin-top: 15px;
+}
 </style>
