baa929fffe0e386a07f393a9372d8141ada1732a
DY12345 <s865529248@163.com>
2020-05-12 13:56:38 +0800

    修复bug

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -200,8 +200,8 @@ export default {
 
       editForm: {},
       editFormRules: {
-        email: [{ required: true, message: '请输入角色名称', trigger: 'blur' }],
-        mobile: [{ required: true, message: '请输入角色描述', trigger: 'blur' }]
+        roleName: [{ required: true, message: '请输入角色名称', trigger: 'blur' }],
+        roleDesc: [{ required: true, message: '请输入角色描述', trigger: 'blur' }]
       }
     }
   },
