30369c7cc516ac4762b1a88cbb484aa9f0936b5a
DY12345 <s865529248@163.com>
2020-05-25 21:30:21 +0800

    在页面顶部添加了加载进度条

--- src/main.js
+++ src/main.js
@@ -9,14 +9,23 @@ import VueQuillEditor from 'vue-quill-editor'
 import 'quill/dist/quill.core.css' // import styles
 import 'quill/dist/quill.snow.css' // for snow theme
 import 'quill/dist/quill.bubble.css' // for bubble theme
+
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
 Vue.use(VueQuillEditor /* { default global options } */)
 Vue.prototype.$http = axios
 // 配置请求的根目录
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 axios.interceptors.request.use(config => {
   // console.log(config)
+  NProgress.start()
   config.headers.Authorization = window.sessionStorage.getItem('token')
   // 最后必须返回config
+  return config
+})
+axios.interceptors.response.use(config => {
+  NProgress.done()
   return config
 })
 Vue.config.productionTip = false
